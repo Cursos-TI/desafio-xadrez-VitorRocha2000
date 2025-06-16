3d4279f17b965de61b387293bb6968e2683b531c
VitorRocha2000 <vr7803849@gmail.com>
2025-06-16 19:52:52 -0300

    Update xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -5,6 +5,7 @@ int main (){
     int torre = 0;
     int bispo = 0;
     int rainha = 0;
+    int cavalo = 0;
 
 
     while (rainha < 8){
@@ -18,17 +19,29 @@ int main (){
         printf("torre cima direita\n");
     }
 
+    
+    
     do
     {
         printf("bispo cima esquerda\n");
         printf("bispo cima direita\n");
         bispo ++;
     } while (bispo < 5);
+
     
     
+    
+    for (cavalo; cavalo < 2; cavalo++){
+        printf("cavalo cima\n");
 
-
-
+        for (cavalo; cavalo < 1; cavalo++)
+        {
+            printf("cavalo cima\n");
+        }
+            printf("cavalo cima direita\n");
+    }
+    
+    
     return 0;
 }
 
@@ -36,10 +49,3 @@ int main (){
 
 
     
-
-
-
-
-
-
-
